a61a848c698c14a4b2e728607f35e2f24a974f43
Wesam <wesamsaleh30@icloud.com>
2019-07-15 14:20:21 -0400

    Comparing KNN and SVM accuracies with classification task

--- svm.py
+++ svm.py
@@ -1,9 +1,11 @@
+#red=1, blue=0
 from sklearn.datasets import make_blobs
 import sklearn as skl
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import svm
-
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
 
 data = make_blobs(n_samples = 10000,n_features=2 ,centers=2)
 features = data[0] 
@@ -17,30 +19,23 @@ coor =[]
 for i in range(10000):
     coor.append([x_coor[i],y_coor[i]])
 
-coor = skl.model_selection.train_test_split(coor, test_size=0.3)
-labels1 = skl.model_selection.train_test_split(labels, test_size = 0.3)
-
-train_data = coor[0]
-test_data = coor[1]
-
-train_lbls = labels1[0]
-test_lbls= labels1[1]
+X_train, X_test, Y_train, Y_test = skl.model_selection.train_test_split(coor,labels, test_size=0.3)
 
 
-clf = svm.SVC(kernel='poly', gamma = .0001, C=1000, degree=3)
-clf.fit(train_data, train_lbls)
+clf = svm.SVC(kernel='poly',degree=3)
+knn = KNeighborsClassifier(n_neighbors=3, algorithm='auto')
 
-acc = 0
-correct = 0
-total = 0
+clf.fit(X_train, Y_train)
+knn.fit(X_train, Y_train)
 
-for i in range(3000):
-    if (clf.predict([test_data[i]]) == test_lbls[i]):
-        correct += 1
-    total+=1
 
-acc = (correct/total)*100
-print("Accuracy: " +str(acc) + "%")
+svm_predictions = clf.predict(X_test)
+knn_predictions = knn.predict(X_test)
+
+print('SVM Accuracy: ' + str(metrics.accuracy_score(svm_predictions,Y_test)*100) + '%')
+print('KNN Accuracy: ' + str(metrics.accuracy_score(knn_predictions,Y_test)*100) + '%')
+
 
 plt.scatter(x_coor,y_coor, c=labels, cmap = 'coolwarm')
+
 plt.show()
